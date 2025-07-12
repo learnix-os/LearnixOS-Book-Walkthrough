7efc3da71347099adddc18cc220f9b30d6941077
sagi21805 <sagi21805@gmail.com>
2025-07-12 16:11:06 +0300

    ch01-02-added-hello-world-print-code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 #![no_std]
 #![no_main]
 
+use core::{arch::asm, panic::PanicInfo};
+
 #[unsafe(no_mangle)]
-fn main() {}
+fn main() {
+    let msg = b"Hello, World!";
+    for &ch in msg {
+        unsafe {
+            asm!(
+                "mov ah, 0x0E",
+                "mov al, {0}",
+                "int 0x10",
+                in(reg_byte) ch,
+                out("ax") _,
+            );
+        }
+    }
+
+    unsafe {
+        asm!("hlt");
+    }
+}
 
 #[panic_handler]
-pub fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
+pub fn panic_handler(_info: &PanicInfo) -> ! {
     loop {}
 }
